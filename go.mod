f87cc7cb7a4355ca6ca7216f39314db608a86ba9
sick-clim <46617682+sick-clim@users.noreply.github.com>
2020-02-26 20:34:23 +0900

    Add go source

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/sick-clim/jwt-sample
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/auth0/go-jwt-middleware v0.0.0-20190805220309-36081240882b
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.7.4
+)
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"net/http"
 
+	"./auth"
+
 	"github.com/gorilla/mux"
+	"github.com/sick-clim/jwt-sample/auth"
 )
 
 type post struct {
@@ -18,11 +21,22 @@ func main() {
 	r := mux.NewRouter()
 	// public でハンドラーを実行
 	r.Handle("/public", public)
+	r.Handle("/private", auth.JwtMiddleware.Handle(private))
+	r.Handle("/auth", auth.GetTokenHandler)
 
 	if err := http.ListenAndServe(":8070", r); err != nil {
 		log.Fatal("ListenAndServe:", nil)
 	}
 }
+
+var private = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "VGolang to Google Cloud Vision",
+		Tag:   "Go",
+		URL:   "https://qiita.com/po3rin/items/bf439424e38757c1e69b",
+	}
+	json.NweEncoder(w).Encode(post)
+})
 
 var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	post := &post{
